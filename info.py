3762074d31375b87120ea78d1ff03e69964f717b
saloni0o <134124882+saloni0o@users.noreply.github.com>
2023-07-01 21:38:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_HASH = environ.get('API_HASH', '')798
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
